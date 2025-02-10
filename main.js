1675ef700d48fcae3a1bd896ee3f5eafd192cf84
Matthew Tubbs <matthtubbs@gmail.com>
2025-02-10 10:31:08 -0700

    created lists obj

---
+++ main.js
@@ -0,0 +1,19 @@
+const lists = {
+    1: {
+        name: "Shopping list",
+        todos: [
+            {
+                text: 'bannanas',
+                completed: false
+            },
+            {
+                text: '1lbs ground turkey',
+                completed: false
+            }
+        ]
+    }
+
+    2: {name: 'Movies with Bailee'}
+}
+
+const currentList = lists[0];
