a3fc4c51daf8ae9a2070a47e2fc8ac0071a6f6e7
Matthew Tubbs <matthtubbs@gmail.com>
2025-02-11 09:43:39 -0700

    created lists array

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 const lists = {
     1: {
-        name: "Shopping list",
-        todos: [
-            {
-                text: 'bannanas',
-                completed: false
-            },
-            {
-                text: '1lbs ground turkey',
-                completed: false
-            }
-        ]
-    }
-
-    2: {name: 'Movies with Bailee'}
-}
-
+      name: "Shopping list",
+      todos: [
+        {
+          text: 'bananas',
+          completed: false
+        },
+        {
+          text: '1 lbs ground turkey',
+          completed: false
+        }
+      ]
+    },
+   }
 const currentList = lists[0];
